0398d1b26e9459d10d8dba2b6a98e1a0ef172cba
rohan gaikwad <rogaikwad30@gmail.com>
2023-04-26 11:40:55 +0530

    added cors

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 const port = 8000;
+
+const allowedOrigins = ['http://localhost:3000'];
+
+const corsOptions = {
+  origin: allowedOrigins,
+};
+
+app.use(cors(corsOptions));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
