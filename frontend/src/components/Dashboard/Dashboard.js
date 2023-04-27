a75d4b46752905d500fc157eab8b03cd4707ce3e
rohan gaikwad <rogaikwad30@gmail.com>
2023-04-27 05:44:43 +0530

    changed dashbaord ui

--- frontend/src/components/Dashboard/Dashboard.js
+++ frontend/src/components/Dashboard/Dashboard.js
@@ -1,18 +1,39 @@
-import React, { useState, useEffect } from 'react';
-import helpers from '../../services/common.service';
+import React, { useState, useEffect, useRef } from "react";
+import helpers from "../../services/common.service";
 import { useSelector } from "react-redux";
+import Glide from "@glidejs/glide";
+import "@glidejs/glide/dist/css/glide.core.min.css";
+import "@glidejs/glide/dist/css/glide.theme.min.css";
+
+import "./Dashboard.css";
 
 function Dashboard() {
   helpers.validateLogin();
+  const carouselRef = useRef(null);
   const user = useSelector((state) => state.user);
   const [blogsByOtherUsers, setBlogsByOtherUsers] = useState([]);
   const [blogsByMe, setBlogsByMe] = useState([]);
 
   useEffect(() => {
-    helpers.getDashboardData(user.token).then(data => {
-      if(data.status === 200){
+    helpers.getDashboardData(user.token).then((data) => {
+      if (data.status === 200) {
         setBlogsByOtherUsers(data.blogs_by_other_users);
         setBlogsByMe(data.blogs_by_me);
+      }
+      if (carouselRef.current) {
+        new Glide(carouselRef.current, {
+          type: "carousel",
+          perView: 3,
+          gap: 20,
+          breakpoints: {
+            768: {
+              perView: 2,
+            },
+            576: {
+              perView: 1,
+            },
+          },
+        }).mount();
       }
     });
   }, [user.token]);
@@ -20,71 +41,146 @@ function Dashboard() {
   const DeleteBlog = async (id) => {
     try {
       const headers = {
-        "access-token": user.token
-      }
+        "access-token": user.token,
+      };
       const resp = await helpers.DeleteBlog(id, headers);
-      if(resp.status === 200){
-        setBlogsByMe(blogsByMe.filter((blog) => {
-          return blog._id !== id
-        }))
+      if (resp.status === 200) {
+        setBlogsByMe(
+          blogsByMe.filter((blog) => {
+            return blog._id !== id;
+          })
+        );
       }
     } catch (error) {
-      console.log("Error Deleting - ",error);
+      console.log("Error Deleting - ", error);
     }
-  }
+  };
 
   const LikeBlog = async (id) => {
     try {
-      const resp = await helpers.LikeBlog(id,user.token)
-      if(resp.status === 200){        
+      const resp = await helpers.LikeBlog(id, user.token);
+      if (resp.status === 200) {
         const data = [];
-        for(let i of blogsByOtherUsers){
-          if(i["_id"] === id){
-            data.push(resp.blog)
-          }
-          else{
-            data.push(i)
+        for (let i of blogsByOtherUsers) {
+          if (i["_id"] === id) {
+            data.push(resp.blog);
+          } else {
+            data.push(i);
           }
         }
-        setBlogsByOtherUsers(data)
+        setBlogsByOtherUsers(data);
       }
     } catch (error) {
-      console.log("Error liking blog - ",error);
+      console.log("Error liking blog - ", error);
     }
-  }
+  };
 
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <h2>Blogs by other users:</h2>
-      <ul>
-        {blogsByOtherUsers.map(blog => (
-          <div key={blog._id}>
-            <a href={"/blog/" + blog._id + "/preview"}>
-              <li>{blog.title}</li>
-            </a>
-            <a style={{"margin":"10px"}} href={"/blog/" + blog._id + "/preview"}>preview</a>
-            <button onClick={() => LikeBlog(blog._id)}>Like - {blog.likes.length}</button>
+    <div className="row">
+      <h1 className="title">Howdy, {user.name}!</h1>
+      <h2>Checkout Latest Blogs by your peers,</h2>
+      {blogsByOtherUsers.map((blog) => (
+        <div key={blog._id} className="example-2 card">
+          <div
+            className="wrapper"
+            style={{ backgroundImage: `url(${blog.imgUrl})` }}
+          >
+            <div className="header">
+              <div className="date">
+                <span className="day">{blog.updatedAt.split("T")[0]}</span>
+                {/* <span className="month">Aug</span>
+          <span className="year">2016</span> */}
+              </div>
+              <ul className="menu-content">
+                <li>
+                  <a href="#" className="fa fa-bookmark-o"></a>
+                </li>
+                <li>
+                  <a
+                    href="#"
+                    onClick={() => LikeBlog(blog._id)}
+                    className="fa fa-thumbs-up"
+                  >
+                    <span>{blog.likes.length}</span>
+                  </a>
+                </li>
+                <li>
+                  <a
+                    href={"/blog/" + blog._id + "/preview"}
+                    className="fa fa-comment"
+                  >
+                    <span>Click</span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="data">
+              <div className="content">
+                <span className="author">{blog.email}</span>
+                <h2 className="title">
+                  <a href={"/blog/" + blog._id + "/preview"}>{blog.title}</a>
+                </h2>
+                <p className="text">{blog.title}</p>
+                <a href={"/blog/" + blog._id + "/preview"} className="button">
+                  Read more
+                </a>
+              </div>
+            </div>
           </div>
-        ))}
-      </ul>
-      <h2>My blogs:</h2>
-      <button>
-        <a href="/blog/new/edit">Add New blog</a>
-      </button>
-      <ul>
-        {blogsByMe.map(blog => (
-          <div key={blog._id}>
-            <a href={"/blog/" + blog._id + "/preview"}>
-              <li>{blog.title}</li>
-            </a>
-            <a style={{"margin":"10px"}} href={"/blog/" + blog._id + "/preview"}>preview</a>
-            <a style={{"margin":"10px"}} href={"/blog/" + blog._id + "/edit"}>edit</a>
-            <button style={{"margin":"10px"}} onClick={() => DeleteBlog(blog._id)}>delete</button>
+        </div>
+      ))}
+
+      <h2>Checkout your own blogs : </h2>
+      {blogsByMe.map((blog) => (
+        <div key={blog._id} className="example-2 card">
+          <div
+            className="wrapper"
+            style={{ backgroundImage: `url(${blog.imgUrl})` }}
+          >
+            <div className="header">
+              <div className="date">
+                <span className="day">{blog.updatedAt.split("T")[0]}</span>
+                {/* <span className="month">Aug</span>
+          <span className="year">2016</span> */}
+              </div>
+              <ul className="menu-content">
+                <li>
+                  {/* <a href="#" className="fa fa-bookmark-o"></a> */}
+                </li>
+                <li>
+                  <p
+                    onClick={() => DeleteBlog(blog._id)}
+                    className="fa fa-trash"
+                  >
+                    <span>{blog.likes.length}</span>
+                  </p>
+                </li>
+                <li>
+                  <a
+                    href={"/blog/" + blog._id + "/edit"}
+                    className="fa fa-pen"
+                  >
+                    <span></span>
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div className="data">
+              <div className="content">
+                <span className="author">{blog.email}</span>
+                <h2 className="title">
+                  <a href={"/blog/" + blog._id + "/preview"}>{blog.title}</a>
+                </h2>
+                <p className="text">{blog.title}</p>
+                <a href={"/blog/" + blog._id + "/preview"} className="button">
+                  Read more
+                </a>
+              </div>
+            </div>
           </div>
-        ))}
-      </ul>
-    </div>
+        </div>
+      ))}
+  </div>
   );
 }
 
